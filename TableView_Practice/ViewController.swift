8393dc9168dcf4f87e1180e94619dbd6b07c295c
JimmyChao90196 <myname90196@gmail.com>
2023-09-13 11:22:29 +0800

    Initial Commit

---
+++ TableView_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView_Practice
+//
+//  Created by JimmyChao on 2023/9/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
